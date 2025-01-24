6a970294443e6e19c3e6d6c9cefaa492d03d2fd6
Ole Schwiegert <ole.schwiegert@oxur.de>
2025-01-24 14:23:27 +0100

    feat: Add cosign and initial packages

--- build.sh
+++ build.sh
@@ -2,6 +2,29 @@
 
 set -ouex pipefail
 
+# Configure SDDM
+dnf install -y sddm qt6-qtsvg qt6-qtvirtualkeyboard qt6-qtmultimedia
+git clone -b master --depth 1 https://github.com/keyitdev/sddm-astronaut-theme.git /usr/share/sddm/themes/sddm-astronaut-theme
+cp -r /usr/share/sddm/themes/sddm-astronaut-theme/Fonts/* /usr/share/fonts/
+echo "[Theme]
+Current=sddm-astronaut-theme" | tee /etc/sddm.conf
+
+echo "[General]
+InputMethod=qtvirtualkeyboard" | tee /etc/sddm.conf.d/virtualkbd.conf
+sed -i 's/ConfigFile=Themes\/astronaut.conf/ConfigFile=Themes\/pixel_sakura.conf/' /usr/share/sddm/themes/sddm-astronaut-theme/metadata.desktop
+
+# Install docker
+dnf -y install dnf-plugins-core
+dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+systemctl enable docker.service
+
+#systemctl enable graphical.target sddm
+
+# Install hyprland
+dnf install -y hyprland
+
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -10,7 +33,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf install -y tmux 
+#dnf install -y tmux
 
 # Use a COPR Example:
 #
@@ -21,4 +44,4 @@ dnf install -y tmux
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+#systemctl enable podman.socket
